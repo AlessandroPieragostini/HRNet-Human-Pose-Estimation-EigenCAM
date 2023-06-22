6b390966e33795f9b4f8d4e545b0014c510dbef0
Vito Scaraggi <s1092648@studenti.univpm.it>
2023-06-22 11:50:12 +0200

    fix config

--- start_train_imagenet.sh
+++ start_train_imagenet.sh
@@ -1,11 +1,11 @@
-python tools/train.py --cfg experiments/babypose/hrnet/3_stage_imagenet.yml &&
+python tools/train.py --cfg experiments/babypose/hrnet/3_stage_imagenet.yaml &&
 mv emissions/emissions.csv emissions/3_stage_imagenet_emissions.csv &&
-python tools/train.py --cfg experiments/babypose/hrnet/4_stage_imagenet.yml &&
+python tools/train.py --cfg experiments/babypose/hrnet/4_stage_imagenet.yaml &&
 mv emissions/emissions.csv emissions/4_stage_imagenet_emissions.csv &&
-python tools/train.py --cfg experiments/babypose/hrnet/4_stage_dist_34_imagenet.yml &&
+python tools/train.py --cfg experiments/babypose/hrnet/4_stage_dist_34_imagenet.yaml &&
 mv emissions/emissions.csv emissions/4_stage_dist_34_imagenet_emissions.csv &&
-python tools/train.py --cfg experiments/babypose/hrnet/4_stage_dist_all4_imagenet.yml &&
+python tools/train.py --cfg experiments/babypose/hrnet/4_stage_dist_all4_imagenet.yaml &&
 mv emissions/emissions.csv emissions/4_stage_dist_all4_imagenet_emissions.csv &&
-python tools/train.py --cfg experiments/babypose/hrnet/4_stage_dist_between_imagenet.yml &&
+python tools/train.py --cfg experiments/babypose/hrnet/4_stage_dist_between_imagenet.yaml &&
 mv emissions/emissions.csv emissions/4_stage_dist_between_imagenet_emissions.csv &&
 echo "all trainings done"
